1620194d918f9e79bbd18c268b7694a2a843d272
Brian Nicholls <nichol88@gmail.com>
2020-01-29 07:39:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    params[:number].to_i.times do
-      params[:phrase]
-    end
+    "#{params[:number].to_i.times { params[:phrase] }}"
   end
 
   get 'say/:word1/:word2/:word3/:word4/:word5' do
