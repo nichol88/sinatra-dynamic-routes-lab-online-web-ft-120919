d88634f72e1255f4b391ccbda09101456aeefaec
Brian Nicholls <nichol88@gmail.com>
2020-01-29 07:37:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,12 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-
+    case params[:operation]
+    when 'multiply'
+    when 'multiply'
+    when 'multiply'
+    when 'multiply'
+    end
   end
 
 end
